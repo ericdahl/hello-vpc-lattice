df8e789f7b10a00683b561835359c86b70e431e1
Eric Dahl <eric.c.dahl@gmail.com>
2025-09-21 11:12:17 -0700

    aws_iam_policy_document data sources

--- vpc_lattice.tf
+++ vpc_lattice.tf
@@ -34,58 +34,60 @@ resource "aws_vpclattice_service_network_vpc_association" "server_goodbye" {
   }
 }
 
+data "aws_iam_policy_document" "demo_service_network_auth" {
+  statement {
+    sid    = "AllowAllVPCs"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["vpc-lattice-svcs:Invoke"]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "vpc-lattice-svcs:SourceVpc"
+      values = [
+        aws_vpc.client.id,
+        aws_vpc.server_hello.id,
+        aws_vpc.server_goodbye.id
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "hello_service_auth" {
+  statement {
+    sid    = "AllowClientVPC"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["vpc-lattice-svcs:Invoke"]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "vpc-lattice-svcs:SourceVpc"
+      values   = [aws_vpc.client.id]
+    }
+  }
+}
+
 resource "aws_vpclattice_auth_policy" "demo" {
   resource_identifier = aws_vpclattice_service_network.demo.arn
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowAllVPCs"
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        }
-        Action = [
-          "vpc-lattice-svcs:Invoke"
-        ]
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "vpc-lattice-svcs:SourceVpc" = [
-              aws_vpc.client.id,
-              aws_vpc.server_hello.id,
-              aws_vpc.server_goodbye.id
-            ]
-          }
-        }
-      }
-    ]
-  })
+  policy              = data.aws_iam_policy_document.demo_service_network_auth.json
 }
 
 resource "aws_vpclattice_auth_policy" "hello_service" {
   resource_identifier = aws_vpclattice_service.hello.arn
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowClientVPC"
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        }
-        Action = [
-          "vpc-lattice-svcs:Invoke"
-        ]
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "vpc-lattice-svcs:SourceVpc" = aws_vpc.client.id
-          }
-        }
-      }
-    ]
-  })
+  policy              = data.aws_iam_policy_document.hello_service_auth.json
 }
